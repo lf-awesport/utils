8c9d381b67c41460027de7a299397fdd92bf3aaf
AWE <office@iMac-di-AWE.local>
2024-07-04 12:58:41 +0200

    retrieve and save articles from cf

--- src/cf-scraper.js
+++ src/cf-scraper.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer")
-const pool = require("es6-promise-pool")
+const Pool = require("es6-promise-pool")
+const axios = require("axios")
 
 const formatDate = (date = new Date()) => {
   var d = new Date(date),
@@ -12,6 +13,40 @@ const formatDate = (date = new Date()) => {
 
   return [year, month, day].join("/")
 }
+
+const promiseProducer = (browser, links, date) => () => {
+  const link = links.pop()
+  return link ? scrapeArticle(browser, link, date) : null
+}
+
+const scrapeArticle = async (browser, link, date) => {
+  const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(0)
+  await page.goto(link)
+
+  const title = await page.$eval(".a-title>h1", (element) => element.innerText)
+  const excerpt = await page.$eval(
+    ".a-excerpt>p",
+    (element) => element.innerText
+  )
+  const body = await page.$$eval(".txt-block>p", (elements) =>
+    elements.map((e) => e.innerText).join("/n")
+  )
+
+  await page.close()
+
+  return saveArticle(title, excerpt, body, date)
+}
+
+const saveArticle = (title, excerpt, body, date, url) =>
+  axios.post("http://localhost:3000/articles", {
+    title,
+    excerpt,
+    body,
+    date,
+    url,
+    id: date + title
+  })
 
 const scraper = async () => {
   const browser = await puppeteer.launch({ headless: true })
@@ -27,11 +62,15 @@ const scraper = async () => {
   )
   // const date = formatDate()
 
-  const date = "2024/07/02"
+  const date = "2024/07/03"
 
   const dailyArticles = articles.filter((a) => a.includes(date))
 
   await page.close()
+
+  const pool = new Pool(promiseProducer(browser, dailyArticles, date), 3)
+  await pool.start()
+
   await browser.close()
 }
 
